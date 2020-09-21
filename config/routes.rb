b16fd124313782879d26287e34b2d796439d6eb4
Jon Roberts <kewlguy@gmail.com>
2020-09-21 14:55:56 -0600

    first commit, model created

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+namespace :api do
+  resources :menus 
+  resources :items
+end
+
+# this one doesnt work below
+  namespace :api do
+  resources :menus do
+    resources :items
+  end
+
+  end
+end
