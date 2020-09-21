c9a90390a988726979f284b3bc2dd56661b2e1f6
Jon Roberts <kewlguy@gmail.com>
2020-09-21 15:16:23 -0600

    controller created and tested api

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,2 +1,22 @@
 class Api::ItemsController < ApplicationController
-end
+
+    def index 
+        render json: Item.all
+    end
+    
+    def create
+    end
+    
+    def update
+    end
+    
+    def destory
+    end
+    
+    private
+    
+    def menu_params
+        params.require(:item).permit(:name, :price)
+    end
+    
+    end
--- app/controllers/api/menus_controller.rb
+++ app/controllers/api/menus_controller.rb
@@ -1,2 +1,22 @@
 class Api::MenusController < ApplicationController
+
+def index 
+    render json: Menu.all
+end
+
+def create
+end
+
+def update
+end
+
+def destory
+end
+
+private
+
+def menu_params
+    params.require(:menu).permit(:name)
+end
+
 end
